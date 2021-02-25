4f6b7e9b68b6c01611e6eb4f49da3b2003e93b83
shrinivasshah <shrinivasshah05@gmail.com>
2021-02-25 14:10:39 +0530

    signals logic

---
+++ signals_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=250, null=True, blank=True)
+    last_name = models.CharField(max_length=250, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
